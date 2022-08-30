b4edd5e6dd7867e13ee4f1bb21bea18d028b8e0c
impulseeeeee <chuikonikita2003@gmail.com>
2022-08-30 19:33:18 +0300

    stats section

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 import "./burger.js";
+import "./sub-menu.js";
---
+++ src/js/sub-menu.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".fact__title").forEach(element => {
+    element.addEventListener("click", (e) => {
+        let content = element.nextElementSibling;
+        e.preventDefault();
+        if (content.style.maxHeight) {
+            document.querySelectorAll('.fact__info').forEach(element => element.style.maxHeight = null);
+        } else {
+            document.querySelectorAll('.fact__info').forEach(element => element.style.maxHeight = null);
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    })
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.code === 'Escape') {
+        document.querySelectorAll('.fact__info').forEach(element => element.style.maxHeight = null)
+    }
+});
